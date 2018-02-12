7663c5ec01f91456a3835045f0da87a8b1c6c243
Hassane <h.sidiammi@gmail.com>
2018-02-12 16:09:41 +0100

    Formation, Form & Validator

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +16,11 @@ class PostType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('title')->add('slug')->add('body')->add('created');
+        $builder
+            ->add('title', TextType::class)
+            ->add('slug', TextType::class)
+            ->add('body', TextareaType::class)
+            ->add('created', DateType::class);
     }/**
      * {@inheritdoc}
      */
