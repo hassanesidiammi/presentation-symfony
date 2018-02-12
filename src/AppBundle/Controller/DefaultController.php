c4f7544e1fddbc2cde8ddae4f5c912075e46b254
Hassane <h.sidiammi@gmail.com>
2018-02-12 16:03:34 +0100

    Formation, Initialisation HttpFondation

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,17 +5,23 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/home", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function homeAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        $view = 'default/index.html.twig';
+        $parameters = [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        ];
+        $response = new Response();
+        $content  = $this->get('twig')->render($view, $parameters);
+        $response->setContent($content);
+
+        return $response;
     }
 }
