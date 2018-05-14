38de829e36456a31e3aee2f3415d1526d30c4987
Hassane <hassane.sidiammi@viseo.com>
2018-05-14 14:07:08 +0200

    Formation, Code review

--- src/AppBundle/Controller/PostController.php
+++ src/AppBundle/Controller/PostController.php
@@ -3,7 +3,9 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Post;
+use AppBundle\Form\PostType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class PostController extends Controller
@@ -11,7 +13,6 @@ class PostController extends Controller
     public function indexAction()
     {
         $em = $this->getDoctrine()->getManager();
-
         $posts = $em->getRepository('AppBundle:Post')->findAll();
 
         return $this->render('post/index.html.twig', array(
@@ -26,15 +27,7 @@ class PostController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $file = $post->getImageFile();
-            if($file) {
-                $fileName = md5(uniqid()).'.'.$file->guessExtension();
-                $file->move(
-                    $this->getParameter('images_directory'),
-                    $fileName
-                );
-                $post->setImage($fileName);
-            }
+            $this->handleImageFile($post, $form);
 
             $em = $this->getDoctrine()->getManager();
             $em->persist($post);
@@ -66,23 +59,7 @@ class PostController extends Controller
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $file = $post->getImageFile();
-
-            if($file) {
-                $fileName = md5(uniqid()).'.'.$file->guessExtension();
-                $file->move(
-                    $this->getParameter('images_directory'),
-                    $fileName
-                );
-                if ($post->getImage()) {
-                    @unlink($this->getParameter('images_directory').'/'.$post->getImage());
-                }
-                $post->setImage($fileName);
-            } elseif ($editForm->has('deleteImage') && $editForm->get('deleteImage')->getData()) {
-                @unlink($this->getParameter('images_directory').'/'.$post->getImage());
-                $post->setImage(null);
-            }
-
+            $this->handleImageFile($post, $editForm);
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('post_edit', array('id' => $post->getId()));
@@ -101,6 +78,7 @@ class PostController extends Controller
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $this->removeImageFile($post);
             $em = $this->getDoctrine()->getManager();
             $em->remove($post);
             $em->flush();
@@ -116,5 +94,52 @@ class PostController extends Controller
             ->setMethod('DELETE')
             ->getForm()
             ;
+    }
+
+    /**
+     * @return string
+     */
+    private function generateUniqueFileName()
+    {
+        return 'post_'.md5(uniqid());
+    }
+
+    /**
+     * @param Post $post
+     * @param PostType $form
+     */
+    private function handleImageFile(Post $post, FormInterface $form)
+    {
+        $file = $post->getImageFile();
+
+        if ($file || $form->has('deleteImage') && $form->get('deleteImage')->getData()) {
+            $this->removeImageFile($post);
+        }
+
+        if ($file) {
+            $fileName = $this->generateUniqueFileName().'.'.$file->guessExtension();
+            $file->move(
+                $this->getParameter('images_directory'),
+                $fileName
+            );
+            $post->setImage($fileName);
+        }
+    }
+
+    /**
+     * @param Post $post
+     *
+     * @return bool
+     */
+    private function removeImageFile(Post $post)
+    {
+        if(!$post->getImage()) {
+            return false;
+        }
+
+        @unlink($this->getParameter('images_directory') . '/' . $post->getImage());
+        $post->setImage(null);
+
+        return true;
     }
 }
